7ffeb45fb7b5465107c00ee3b95bceaa2b2f65c5
gaoliang <gaoliang@360.cn>
2023-11-10 10:10:06 +0800

    实现了mysqlbinlog时间戳消费、指定编码格式消费

--- chunjun-local-test/src/main/resources/oracle.sql
+++ chunjun-local-test/src/main/resources/oracle.sql
@@ -1,24 +1,26 @@
 CREATE TABLE source
-(   `T1` DATE,
- 	`T2` TIMESTAMP (6),
- 	`T3` TIMESTAMP (9),
- 	`T4` TIMESTAMP (6)
+(   `id` int,
+ 	`username` varchar,
+ 	`age` int
 ) WITH (
-      'schema' = 'TEST',
-      'scan.fetch-size' = '1024',
-      'password' = 'oracle',
-      'connector' = 'oracle-x',
-      'scan.query-timeout' = '600',
-      'table-name' = 'ALL_DATA_TYPES_DATE',
-      'url' = 'jdbc:oracle:thin:@p70197v.hulk.bjzdt.qihoo.net:1521:ORCL',
-      'username' = 'system'
+  'connector' = 'binlog-x'
+      ,'username' = 'root'
+      ,'password' = '11111111'
+      ,'cat' = 'insert,delete,update'
+      ,'url' = 'jdbc:mysql://10.17.31.234:3306/360test'
+      ,'host' = '10.17.31.234'
+      ,'port' = '3306'
+      -- 什么都不加：最新位置消费
+      -- 加文件名，从此文件开头消费
+      -- ,'journal-name' = 'binlog.000194'
+        ,'timestamp'='1699447812000'
+      ,'table' = '360test.dimension_table'
+      ,'timestamp-format.standard' = 'SQL'
       );
 CREATE TABLE sink
-(
-  `T1` DATE,
- 	`T2` TIMESTAMP (6),
- 	`T3` TIMESTAMP (9),
- 	`T4` TIMESTAMP (6)
+(   `id` int,
+ 	`username` varchar,
+ 	`age` int
 ) WITH (
        'connector' = 'print'
       );
