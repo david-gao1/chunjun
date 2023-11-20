fc4d40ca4b008c5d1ea43f0baaedcb820169d289
gaoliang <gaoliang@360.cn>
2023-11-20 15:12:07 +0800

    flink kafka upsert 能力

--- chunjun-local-test/src/main/resources/oracle.sql
+++ chunjun-local-test/src/main/resources/oracle.sql
@@ -1,43 +1,94 @@
-CREATE TABLE source
-(   `id` int,
- 	`username` varchar,
- 	`age` int
-) WITH (
-  'connector' = 'binlog-x'
-      ,'username' = 'root'
-      ,'password' = '11111111'
-      ,'cat' = 'insert,delete,update'
-      ,'url' = 'jdbc:mysql://10.17.31.234:3306/360test'
-      ,'host' = '10.17.31.234'
-      ,'port' = '3306'
-      -- 什么都不加：最新位置消费
-      -- 加文件名，从此文件开头消费
-       ,'journal-name' = 'binlog.000194'
-      --  ,'timestamp'='169944781200'
-      ,'table' = '360test.dimension_table'
-      ,'timestamp-format.standard' = 'SQL'
-      );
-CREATE TABLE sink
-(   `id` int,
- 	`name` varchar,
- 	`age` int,
- 	PRIMARY KEY (id,name) NOT ENFORCED
-) WITH (
-        'connector' = 'mysql-x',
-           'url' = 'jdbc:mysql://localhost:3306/360test',
-           'table-name' = 'test003',
-           'username' = 'root',
-           'password' = '11111111',
-           'sink.buffer-flush.max-rows' = '1024', -- 批量写数据条数，默认：1024
-           'sink.buffer-flush.interval' = '10000', -- 批量写时间间隔，默认：10000毫秒
-           -- insert时的选项，覆盖或者忽略。
-           -- 声明了主键时，设置all-replace为true，全部更新覆盖，
-           -- 或者是忽略，即来的新数据不插入？
-           'sink.all-replace' = 'true', -- 解释如下(其他rdb数据库类似)：默认：false。定义了PRIMARY KEY才有效，否则是追加语句
-                                       -- sink.all-replace = 'true' 生成如：INSERT INTO `result3`(`mid`, `mbb`, `sid`, `sbb`) VALUES (?, ?, ?, ?) ON DUPLICATE KEY UPDATE `mid`=VALUES(`mid`), `mbb`=VALUES(`mbb`), `sid`=VALUES(`sid`), `sbb`=VALUES(`sbb`) 。会将所有的数据都替换。
-                                       -- sink.all-replace = 'false' 生成如：INSERT INTO `result3`(`mid`, `mbb`, `sid`, `sbb`) VALUES (?, ?, ?, ?) ON DUPLICATE KEY UPDATE `mid`=IFNULL(VALUES(`mid`),`mid`), `mbb`=IFNULL(VALUES(`mbb`),`mbb`), `sid`=IFNULL(VALUES(`sid`),`sid`), `sbb`=IFNULL(VALUES(`sbb`),`sbb`) 。如果新值为null，数据库中的旧值不为null，则不会覆盖。
-           -- 新增写入选项：默认会判断,当声明了key则是update
-           'sink.parallelism' = '1'    -- 写入结果的并行度，默认：null
-      );
-insert into sink select id,username as name,age as age  from source;
+-- {"id":100,"name":"lb james阿道夫","money":293.899778,"dateone":"2020-07-30 10:08:22","age":"33","datethree":"2020-07-30 10:08:22.123","datesix":"2020-07-30 10:08:22.123456","datenigth":"2020-07-30 10:08:22.123456789","dtdate":"2020-07-30","dttime":"10:08:22"}
+-- {"id":30,"name":"aaa"}|||{"id":30,"name":"aaa"}
+-- {"id":40,"name":"aaa"}|||{"id":40,"name":"aaa"}
+-- {"id":50,"name":"bbb"}|||{"id":50,"name":"bbb"}
+-- {"id":50,"name":"bbb"}|||{"id":50,"name":"bbb"}
+-- {"id":60,"name":"ccc"}|||{"id":60,"name":"ccc"}
+CREATE TABLE source_ods_fact_user_ippv
+(
+    id           INT,
+    name         STRING
+--     ,money        decimal,
+--     dateone      timestamp,
+--     age          bigint,
+--     datethree    timestamp(3),
+--     datesix      timestamp(6),
+--     datenigth    timestamp(9),
+--     dtdate       date,
+--     dttime       time
 
+--     ,ts           TIMESTAMP(3) METADATA FROM 'timestamp',
+--     partition_id BIGINT METADATA FROM 'partition' VIRTUAL, -- from Kafka connector
+--     WATERMARK FOR datethree AS datethree - INTERVAL '5' SECOND
+) WITH (
+      'connector' = 'kafka-x'
+      ,'topic' = 'logsget_middle_di_test'
+      ,'properties.bootstrap.servers' = '10.224.144.18:39092'
+      ,'properties.group.id' = 'middle_local_test_0325be2d08b3eeaa3d19dae1a0529a14'
+--       ,'scan.startup.mode' = 'earliest-offset'
+      ,'scan.startup.mode' = 'latest-offset'
+      ,'format' = 'json'
+      ,'json.timestamp-format.standard' = 'SQL'
+      -- ,'scan.parallelism' = '3'
+      );
+
+
+CREATE TABLE result_total_pvuv_min
+(
+    id           INT,
+    name         STRING
+--     ,money        decimal,
+--     dateone      timestamp,
+--     age          bigint,
+--     datethree    timestamp(3),
+--     datesix      timestamp(6),
+--     datenigth    timestamp(9),
+--     dtdate       date,
+--     dttime       time,
+
+--     ts           TIMESTAMP(3),
+--     partition_id BIGINT,
+--     PRIMARY KEY (id, name, money, dateone, age, datethree, datesix, datenigth, dtdate, dttime, ts,
+--                  partition_id) NOT ENFORCED
+
+    ,PRIMARY KEY (id) NOT ENFORCED
+) WITH (
+--       'connector' = 'stream-x'
+
+      'connector' = 'upsert-kafka-x'
+      ,'topic' = 'logsget_middle_di_test_join'
+      ,'properties.bootstrap.servers' = '10.224.144.18:39092'
+      ,'key.format' = 'json'
+      ,'value.format' = 'json'
+      ,'value.fields-include' = 'ALL'
+      -- ,'sink.parallelism' = '2'
+      );
+
+
+INSERT INTO result_total_pvuv_min
+SELECT id
+     , name
+--      , money
+--      , dateone
+--      , age
+--      , datethree
+--      , datesix
+--      , datenigth
+--      , dtdate
+--      , dttime
+--      , ts
+--      , partition_id
+from source_ods_fact_user_ippv
+group by id
+       , name
+--        , money
+--        , dateone
+--        , age
+--        , datethree
+--        , datesix
+--        , datenigth
+--        , dtdate
+--        , dttime
+--        , ts
+--        , partition_id
+;
